5af054724b513c420f4938969244a7656d86dbda
hritik kumar <hritik.7827@gmail.com>
2025-11-19 16:50:07 +0530

    added all the dependencies

---
+++ src/main/java/com/server/avista/AvistaApplication.java
@@ -0,0 +1,13 @@
+package com.server.avista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/avista/AvistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.avista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
